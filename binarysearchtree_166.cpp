566061cac1405762a1cfbcfc0d4e15af369c1f1f
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2023-06-29 12:42:46 +0700

    membuat left child node menjadi null

--- binarysearchtree_166.cpp
+++ binarysearchtree_166.cpp
@@ -29,6 +29,8 @@ public:
 	void insert(string element) //insert a node to binary search tree
 	{
 		Node* NewNode =  new Node(element, NULL, NULL);  //ALCOATE MEMORY FOR THE NEW NODE
-		newNode->info = element;	//assign value to the data field of the new node
+		NewNode->info = element;	//assign value to the data field of the new node
+		NewNode->leftchild = NULL;	//MAKE THE LEFT CHILD OF THE NEWNODE POINT TO NULL
+		
 	}
 };
